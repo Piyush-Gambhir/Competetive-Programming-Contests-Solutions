ede896bcc231d654a604275bb16fdfe7ccdeebeb
Piyush Gambhir <90608533+Piyush-Gambhir@users.noreply.github.com>
2023-08-08 19:41:40 +0530

    Added Solutions

---
+++ Codeforces/Educational Codeforces Round/Educational Codeforces Round 60 (Rated for Div. 2)/C. Magic Ship.java
@@ -0,0 +1,11 @@
+/*
+Problem Link: https://codeforces.com/problemset/problem/1117/C
+*/
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        
+    }
+}
